774227f343aa8fbc13bf3a8c14180ddcaee873a8
pashioya <paul.ashioya@student.kdg.be>
2023-05-23 10:40:30 +0200

    front end thymeleaf

--- src/main/java/lootbox/controller/AdvertisementController.java
+++ src/main/java/lootbox/controller/AdvertisementController.java
@@ -1,6 +1,7 @@
 package lootbox.controller;
 
 import lombok.AllArgsConstructor;
+import lootbox.domain.Advertisement;
 import lootbox.repository.AdvertisementRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,18 +9,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/advertisements")
+@RequestMapping("/")
 public class AdvertisementController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final AdvertisementRepository advertisementRepo;
 
     @GetMapping()
-    public String showAllAds (Model model) {
+    public ModelAndView showAllAds () {
         logger.info("Showing All Ads");
-        model.addAttribute("advertisements", advertisementRepo.findAll());
-        return "index";
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("advertisements", advertisementRepo.findAll());
+        return modelAndView;
     }
 }
--- src/main/java/lootbox/domain/Advertisement.java
+++ src/main/java/lootbox/domain/Advertisement.java
@@ -2,13 +2,11 @@ package lootbox.domain;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
+@ToString
 @NoArgsConstructor
 @Getter
 @Setter
