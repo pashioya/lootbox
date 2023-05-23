08e8e7f98aadf4b42e583081ca089bb1575b4cf8
pashioya <paul.ashioya@student.kdg.be>
2023-05-23 09:02:49 +0200

    initial commit 2

---
+++ src/test/java/kdg/be/lootbox/LootBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package kdg.be.lootbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
