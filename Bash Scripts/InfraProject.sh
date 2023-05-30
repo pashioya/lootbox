ab17a941b28fe73ddf30cdf288d296fcec1dfba5
Paul Ashioya <John.ashioya@gmail.com>
2023-05-30 22:43:14 +0200

    updating scripts

--- Bash Scripts/InfraProject.sh
+++ Bash Scripts/InfraProject.sh
@@ -1,91 +1,79 @@
 #!/bin/bash
 
-# Description	: This script deploys a web app on a newly instantiated VM and Cloud SQL database. [ config dhcp and dns ... ]
+# Description	: This script deploys a web app on a newly instantiated VM and Cloud SQL database on a VPC and configures DNS
 
 # Set variables
 PROJECT_ID="infra3-leemans-freddy"
-BUCKET_NAME="bucket-$(date +%s)"
-VM_NAME="project-vm-$(date +%s)"
+BUCKET_NAME="lootbox-bucketeu"
+VM_NAME="lootbox-vm-1"
 ZONE="europe-west1-b"
 MACHINE_TYPE="e2-standard-2"
 IMAGE_FAMILY="ubuntu-2204-lts"
 IMAGE_PROJECT="ubuntu-os-cloud"
 TAGS="http-server"
-STARTUP_SCRIPT="#!/bin/bash
+EXT_IP_ADDRESS="lootbox-vm-static"
+STARTUP_SCRIPT='#!/bin/bash
+
+# Install dependencies
 sudo apt-get update
-sudo apt-get install -y apache2
-sudo apt-get install ufw
-sudo ufw --force enable
-sudo ufw allow 22/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo apt-get install -y postgresql-client
-gsutil -m cp -r gs://$BUCKET_NAME/* /var/www/html"
-DB_NAME="postgres-db-$(date +%s)"
-DB_SERVER_NAME="postgres-sql-$(date +%s)"
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt upgrade -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get install -y ufw docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enable and configure UFW
+ufw --force enable
+ufw allow 22/tcp
+ufw allow 80/tcp'
+DB_NAME="postgres-sql-1684826042eu"
+DB_SERVER_NAME="postgres-sql-1684825152eu"
 DB_TIER="db-g1-small"
 REGION="europe-west1"
 DB_VERSION="POSTGRES_14"
-USER="user-$(date +%s)"
-VPC_NAME="vpc-$(date +%s)"
-SUBNET_NAME="subnet-$(date +%s)"
-FIREWALL_RULE="web-traffic"
-PORTS="tcp:80,tcp:443"
+USER="lootbox-db-eu"
+VPC_NAME="lootbox-vpceu"
+SUBNET_NAME="lootbox-subneteu"
+SUBNET_RANGE="10.0.0.0/28"
+FIREWALL_RULE="web-traffic-ssh-traffic"
+PORTS="tcp:22,tcp:80,tcp:443"
+DNS_ZONE="lootbox-store"
+DNS_NAME="www.lootbox-store.site."
 
 # Creates the Cloud SQL instance
-create_sql() { 
+create_sql() {
 	echo "Creating Cloud SQL instance..."
 	gcloud sql instances create $DB_SERVER_NAME \
 		--project=$PROJECT_ID \
 		--tier=$DB_TIER \
 		--region=$REGION \
-		--authorized-networks=0.0.0.0/0 \
+		--network=$VPC_NAME \
 		--database-version=$DB_VERSION \
+		--authorized-networks=0.0.0.0/0 \
 		--root-password=root
 }
 
 # Create Cloud database and user
-create_db() { 
+create_db() {
 	echo "Creating database and user..."
 	gcloud sql databases create $DB_NAME \
 		--instance=$DB_SERVER_NAME
 	read -s -p "Enter password for $USER: " passwd
 	gcloud sql users create $USER --instance=$DB_SERVER_NAME --password=$passwd
-	
-	echo "Setting up db structure..." # TODO
-	gcloud sql connect $DB_SERVER_NAME --user=$USER --password=$passwd --quiet \
-		--project=$PROJECT_NAME --database=$DB_NAME <<EOF
-CREATE TABLE public.advertisement
-(
-    id           bigserial PRIMARY KEY,
-    description  varchar(255) NOT NULL,
-    email        varchar(255) NOT NULL,
-    image        varchar(255) NOT NULL,
-    phone_number varchar(255) NOT NULL,
-    title        varchar(255) NOT NULL
-);
-
-ALTER TABLE public.advertisement OWNER TO postgres;
-
-);
-EOF
-	exit
 }
 
 # Clone the web page repo, Create Cloud Storage bucket and upload the static files
-create_bucket() { 
+create_bucket() {
 	echo "Creating storage bucket..."
-	gsutil mb -p $PROJECT_ID gs://$BUCKET_NAME
+	gsutil mb -l $REGION -p $PROJECT_ID gs://$BUCKET_NAME
 	gsutil iam ch allUsers:objectViewer gs://$BUCKET_NAME
-	
-	echo "Downloading git repo..."
-	git clone https://gitlab.com/noah.diderich/infra3_project.git
-	cd infra3_project/src/main/resources/
-	gsutil -m rsync -r . gs://$BUCKET_NAME
 }
 
 # Creates the VM
-create_vm() { 
+create_vm() {
 	echo "Creating VM..."
 	gcloud compute instances create $VM_NAME \
 		--project=$PROJECT_ID \
@@ -95,12 +83,14 @@ create_vm() {
 		--image-project=$IMAGE_PROJECT \
 		--network=$VPC_NAME \
 		--subnet=$SUBNET_NAME \
-		--private-network-ip="10.0.0.10"
+		--address=$EXT_IP_ADDRESS \
+		--private-network-ip="10.0.0.2" \
+		--tags=$TAGS \
 		--metadata="startup-script=$STARTUP_SCRIPT"
 }
 
 # Creates and configures the VPC
-create_vpc() { 
+create_vpc() {
 	echo "Creating VPC network..."
 	gcloud compute networks create $VPC_NAME \
 		--project=$PROJECT_ID \
@@ -111,7 +101,7 @@ create_vpc() {
 	       	--project=$PROJECT_ID \
 		--region=$REGION \
 		--network=$VPC_NAME \
-		--range=10.0.0.0/24
+		--range=$SUBNET_RANGE
 
 	gcloud compute firewall-rules create $FIREWALL_RULE \
 		--project=$PROJECT_ID \
@@ -123,22 +113,39 @@ create_vpc() {
 	gcloud compute networks subnets update $SUBNET_NAME \
 		--project=$PROJECT_ID \
 		--region=$REGION \
-		--add-secondary-ranges=private-range=10.0.1.0/24
+		--add-secondary-ranges=private-range=10.0.1.0/28
 }
 
 # Set up private connection from VM to SQL
-setup_private() { 
+setup_private() {
 	echo "Establish private connection from VM to SQL..."
 	gcloud sql instances patch $DB_SERVER_NAME \
 		--project=$PROJECT_ID \
-		--network=$VPC_NAME \
-		--no-assign-ip \
-		--authorized-networks=10.0.0.10/32
+		--require-ssl \
+		--authorized-networks=10.0.0.2/32 \
+		--quiet
 }
 
+# Create DNS zone and configure DNS settings
+setup_dns() {
+	echo "Setting up DNS..."
+	gcloud dns --project=$PROJECT_ID managed-zones create $DNS_ZONE \
+		--description="" \
+		--dns-name=$DNS_NAME \
+		--visibility="public" \
+		--dnssec-state="off"
+
+	gcloud dns --project=$PROJECT_ID record-sets update $DNS_NAME \
+		--type="A" \
+		--zone=$DNS_ZONE \
+		--rrdatas=$EXT_IP_ADDRESS \
+		--ttl="300"
+}
+
+#create_vpc
 #create_sql
 #create_db
 #create_bucket
-create_vpc
-create_vm
+#create_vm
 setup_private
+#setup_dns
